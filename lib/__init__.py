d043132bfeef78f1e30f5f7a4ba1c37e066fa6d8
beuve <nicolas@beuve.net>
2026-09-05 12:27:26 +0200

    [fix] fix automatic version in pyproject

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,8 @@
 from .decorators import proxy_test_decorator, Test, test
 from .registration import discover_tests
 from .formater import format_failed
+
+try:
+    from surimi._version import __version__
+except ImportError:
+    __version__ = "0.0.0"
